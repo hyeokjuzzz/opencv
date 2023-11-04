db39453ef710f86a5d6f6af9ead35b3abaf8037d
hyeokjuzzz <140710676+hyeokjuzzz@users.noreply.github.com>
2023-11-04 22:21:57 +0900

    Create Color detection02.cs

---
+++ Day20231027/Color detection02.cs
@@ -0,0 +1,24 @@
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5._2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Mat src = Cv2.ImRead("crow.jpg");
+			Mat dst = new Mat(src.Size(), MatType.CV_8UC3);
+
+			Cv2.CvtColor(src, dst, ColorConversionCodes.BGR2HSV);
+
+			Cv2.ImShow("dst", dst);
+			Cv2.WaitKey();
+			Cv2.DestroyAllWindows();
+		}
+	}
+}
